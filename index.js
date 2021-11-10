78ce8d67bb17bf6e99fb14fa73bc9d55f0c903a4
KARIMANSHAWKI <kariman0800@gmail.com>
2021-11-10 03:44:18 +0200

    add categories && upload image api

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const uploadImage = require("./services/store-image");
+
 
 // MIDDLEWARES
 dotenv.config();
@@ -23,6 +26,8 @@ mongoose
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+app.use("/api/upload-image", uploadImage);
 
 app.listen("5000", () => {
   console.log("Server is running ....");
---
+++ services/store-image.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const hasWhiteSpaceFun = require('../services/has-white-space');
+const router = require("express").Router();
+
+
+router.post("/", (req, res) => {
+  let originalName =  req.file.name;
+  let fileName = '';
+  if (hasWhiteSpaceFun.hasWhiteSpace(originalName)) {
+    let fileWithoutSpace = originalName.split(" ").join("");
+      fileName =  Date.now() + "-" + fileWithoutSpace
+  } else {
+     fileName = Date.now() + "-" + originalName;
+  }
+
+  let filePath = __dirname + "/images/" + fileName;
+  file.mv(filePath);
+  res.status(200).json("Image uploaded successfully");
+});
+
+
+
+
+module.exports = router;
